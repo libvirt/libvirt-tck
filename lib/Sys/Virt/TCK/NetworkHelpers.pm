604b3824cae7fe3009025aeaa4bf57bd9cddcea7
Laine Stump <laine@laine.org>
2018-02-28 12:48:16 -0500

    new NetworkHelper function get_network_ip()
    This function gets the first IP address for the named virtual
    network. It is returned as a Net::IP object, so that we will have info
    about its netmask/prefix and can easily get it broadcast address and
    perform arithmetic on the address.
    
    Signed-off-by: Laine Stump <laine@laine.org>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>

--- lib/Sys/Virt/TCK/NetworkHelpers.pm
+++ lib/Sys/Virt/TCK/NetworkHelpers.pm
@@ -1,4 +1,5 @@
 use Sys::Virt::TCK qw(xpath);
+use NetAddr::IP qw(:lower);
 use strict;
 use utf8;
 
@@ -8,6 +9,27 @@ sub get_first_macaddress {
     utf8::encode($mac);
     return $mac;
 }
+
+sub get_network_ip {
+    my $conn = shift;
+    my $netname = shift;
+    diag "getting ip for network $netname";
+    my $net = $conn->get_network_by_name($netname);
+    my $net_ip = xpath($net, "string(/network/ip[1]/\@address");
+    my $net_mask = xpath($net, "string(/network/ip[1]/\@netmask");
+    my $net_prefix = xpath($net, "string(/network/ip[1]/\@prefix");
+    my $ip;
+
+    if ($net_mask) {
+        $ip = NetAddr::IP->new($net_ip, $net_mask);
+    } elsif ($net_prefix) {
+        $ip = NetAddr::IP->new("$net_ip/$net_mask");
+    } else {
+        $ip = NetAddr::IP->new("$net_ip");
+    }
+    return $ip;
+}
+
 
 sub get_ip_from_leases{
     my $conn = shift;

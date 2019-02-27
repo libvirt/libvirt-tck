libvirt/libvirt-tck

commit c1af9677491d41c8d7a6cc741674e234796de6d0

Change 'Sys::Virt::TCK::generic_domain' to take named params
To make it easier to add more optional parameters to the
Sys::Virt::TCK::generic_domain method, change it to take
named parameters instead of positional parameters.

Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

lib/Sys/Virt/TCK.pm
@@ -607,14 +607,14 @@ sub get_kernel {
 
 sub generic_machine_domain {
     my $self = shift;
-    my $name = shift;
-    my $caps = shift;
-    my $ostype = shift;
-    my $conn = @_ ? shift : $self->conn;
+    my %params = @_;
+    my $name = exists $params{name} ? $params{name} : "tck";
+    my $ostype = exists $params{ostype} ? $params{ostype} : "hvm";
+    my $caps = exists $params{caps} ? $params{caps} : die "caps parameter is required";
 
     my %config = $self->get_kernel($caps, $ostype);
 
-    my $b = Sys::Virt::TCK::DomainBuilder->new(conn => $conn,
+    my $b = Sys::Virt::TCK::DomainBuilder->new(conn => $self->conn,
 					       name => $name,
 					       domain => $config{domain},
 					       ostype => $config{ostype});
@@ -652,14 +652,14 @@ sub best_container_domain {
 
 sub generic_container_domain {
     my $self = shift;
-    my $name = shift;
-    my $caps = shift;
-    my $domain = shift;
-    my $conn = @_ ? shift : $self->conn;
+    my %params = @_;
+    my $name = exists $params{name} ? $params{name} : "tck";
+    my $caps = exists $params{caps} ? $params{caps} : die "caps parameter is required";
+    my $domain = exists $params{domain} ? $params{domain} : die "domain parameter is required";
 
     my $bucket = "os-exe";
 
-    my $b = Sys::Virt::TCK::DomainBuilder->new(conn => $conn,
+    my $b = Sys::Virt::TCK::DomainBuilder->new(conn => $self->conn,
 					       name => $name,
 					       domain => $domain,
 					       ostype => "exe");
@@ -679,11 +679,12 @@ sub generic_container_domain {
 
 sub generic_domain {
     my $self = shift;
-    my $name = @_ ? shift : "tck";
-    my $ostype = @_ ? shift : undef;
-    my $conn = shift || $self->conn;
+    my %params = @_;
 
-    my $caps = Sys::Virt::TCK::Capabilities->new(xml => $conn->get_capabilities);
+    my $name = exists $params{name} ? $params{name} : "tck";
+    my $ostype = exists $params{ostype} ? $params{ostype} : "hvm";
+
+    my $caps = Sys::Virt::TCK::Capabilities->new(xml => $self->conn->get_capabilities);
 
     my $container;
 
@@ -691,9 +692,13 @@ sub generic_domain {
 	unless $ostype && $ostype ne "exe";
 
     if ($container) {
-	return $self->generic_container_domain($name, $caps, $container);
+	return $self->generic_container_domain(name => $name,
+					       caps => $caps,
+					       domain => $container);
     } else {
-	return $self->generic_machine_domain($name, $caps, $ostype);
+	return $self->generic_machine_domain(name => $name,
+					     caps => $caps,
+					     ostype => $ostype);
     }
 }
 

docs/writing-tests.pod
@@ -159,7 +159,7 @@ the exact config, just wanting a minimal generic domain config that is
 highly likely to work without error. For such cases, a nice simple
 API is provided:
 
-   my $xml = $tck->generic_domain("test")->as_xml;
+   my $xml = $tck->generic_domain(name => "test")->as_xml;
 
 This creates an XML document for a guest that is of the correct OS and
 domain type to be able to run on the current hypervisor, with a name
@@ -167,7 +167,7 @@ of 'test', and a single disk. It is possible to set further parameters
 if required. For example, to set an explicit UUID, give 3 virtual CPUs
 and turn on ACPI:
 
-  my $xml = $tck->generic_domain("test")->vcpus(3)
+  my $xml = $tck->generic_domain(name => "test")->vcpus(3)
              ->uuid("11111111-1111-2222-3333-444444444444")
              ->with_acpi()->as_xml()
 
@@ -357,7 +357,7 @@ Taking this into account, the complete example script looks like
     END { $tck->cleanup if $tck; }
 
 
-    my $xml = $tck->generic_domain("test")->as_xml;
+    my $xml = $tck->generic_domain(name => "test")->as_xml;
 
     my $dom;
     diag "Defining inactive domain config again";

lib/Sys/Virt/TCK/NetworkHelpers.pm
@@ -58,7 +58,7 @@ sub build_domain{
 
     my ($cdrom, $ksurl) = build_cdrom_ks_image($tck);
 
-    my $guest = $tck->generic_domain($domain_name);
+    my $guest = $tck->generic_domain(name => $domain_name);
 
     # change the type of network connection for 802.1Qbg tests
     if ($mode eq  "vepa") {

scripts/domain/050-transient-lifecycle.t
@@ -40,7 +40,7 @@ BAIL_OUT "failed to setup test harness: $@" if $@;
 END { $tck->cleanup if $tck; }
 
 
-my $xml = $tck->generic_domain("tck")->as_xml;
+my $xml = $tck->generic_domain(name => "tck")->as_xml;
 
 diag "Creating a new transient domain";
 my $dom;

scripts/domain/051-transient-autostart.t
@@ -38,7 +38,7 @@ BAIL_OUT "failed to setup test harness: $@" if $@;
 END { $tck->cleanup if $tck; }
 
 
-my $xml = $tck->generic_domain("tck")->as_xml;
+my $xml = $tck->generic_domain(name => "tck")->as_xml;
 
 diag "Creating a new transient domain";
 my $dom;

scripts/domain/060-persistent-lifecycle.t
@@ -39,7 +39,7 @@ BAIL_OUT "failed to setup test harness: $@" if $@;
 END { $tck->cleanup if $tck; }
 
 
-my $xml = $tck->generic_domain("tck")->as_xml;
+my $xml = $tck->generic_domain(name => "tck")->as_xml;
 
 diag "Defining an inactive domain config";
 my $dom;

scripts/domain/061-persistent-autostart.t
@@ -39,7 +39,7 @@ BAIL_OUT "failed to setup test harness: $@" if $@;
 END { $tck->cleanup if $tck; }
 
 
-my $xml = $tck->generic_domain("tck")->as_xml;
+my $xml = $tck->generic_domain(name => "tck")->as_xml;
 
 diag "Defining an inactive domain config";
 my $dom;

scripts/domain/065-persistent-redefine.t
@@ -38,7 +38,7 @@ BAIL_OUT "failed to setup test harness: $@" if $@;
 END { $tck->cleanup if $tck; }
 
 
-my $cfg = $tck->generic_domain("tck")->uuid("11111111-1111-1111-1111-111111111111");
+my $cfg = $tck->generic_domain(name => "tck")->uuid("11111111-1111-1111-1111-111111111111");
 $cfg->on_reboot("restart");
 my $xml1 = $cfg->as_xml;
 $cfg->on_reboot("destroy");

commit 7c5e997d53e9af163398f4e17aea5b9fc07b6e2c

Don't set a default value for ostype
We must leave ostype undefined by default so that we can auto-detect the
right ostype based on reported capabilities. This fixes the ability to
run tests with the LXC driver previously broken in

  commit c1af9677491d41c8d7a6cc741674e234796de6d0
  Author: Daniel P. Berrange <berrange@redhat.com>
  Date:   Thu Mar 27 11:57:14 2014 +0000

    Change 'Sys::Virt::TCK::generic_domain' to take named params

Reviewed-by: Laine Stump <laine@laine.org>
Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

lib/Sys/Virt/TCK.pm
@@ -919,7 +919,7 @@ sub generic_domain {
     my %params = @_;
 
     my $name = exists $params{name} ? $params{name} : "tck";
-    my $ostype = exists $params{ostype} ? $params{ostype} : "hvm";
+    my $ostype = exists $params{ostype} ? $params{ostype} : undef;
     my $fullos = exists $params{fullos} ? $params{fullos} : 0;
     my $netmode = exists $params{netmode} ? $params{netmode} : undef;
     my $shareddisk = exists $params{shareddisk} ? $params{shareddisk} : 0;